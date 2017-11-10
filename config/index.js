2735b96b0f91a15dc36c8e64473619870e3a2daa
Keith C. Ivey <keith@iveys.org>
2017-11-10 17:33:25 -0500

    allow loading config from environment variables and args

--- config/index.js
+++ config/index.js
@@ -1,7 +1,17 @@
 var path = require('path'),
-    nconf = require('nconf');
+    nconf = require('nconf'),
+    camelize = require('underscore.string/camelize'),
+    keys = Object.keys(require('./defaults.json'));
 
-nconf.file('local', {file: path.join(__dirname, 'local.json')})
+nconf.argv()
+    .env({
+        parseValues: true,
+        transform: function (obj) {
+            obj.key = camelize(obj.key.toLowerCase());
+            return keys.indexOf(obj.key) === -1 ? false : obj;
+        }
+    })
+    .file('local', {file: path.join(__dirname, 'local.json')})
     .file('defaults', {file: path.join(__dirname, 'defaults.json')});
 
 module.exports = nconf.get(); // whole config object
